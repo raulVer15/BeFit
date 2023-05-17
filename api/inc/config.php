52a67f84e94ea949e17353571052f6d1083280fc
Nathan Rodriguez <nrodriguez1498@gmail.com>
2023-05-16 20:24:20 -0700

    Fixed config.php

--- api/inc/config.php
+++ api/inc/config.php
@@ -1,7 +1,7 @@
 <?php
 	define("DB_HOST", "befit-sql-db-do-user-13809763-0.b.db.ondigitalocean.com");
 	define("DB_USERNAME", "doadmin");
-	define("DB_PASSWORD", "AVNS_Jl0HMzG_TfOv4zqRwaO");
+	define("DB_PASSWORD", "AVNS_JlOHMzG_TfOv4zqRwa0");
 	define("DB_NAME", "befit");	
 	define("PORT", "25060");
 
